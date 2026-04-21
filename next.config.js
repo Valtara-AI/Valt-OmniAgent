cade1c33bc8e1f9325cda0af5f6f0bab91ef4ccf
blu_geek <fogboe@gmail.com>
2026-04-20 22:53:38 -0600

    Fix: Overriding install command via vercel.json to resolve React 18 conflict

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // We removed the 'target' line entirely to escape Legacy Mode
   images: {
     unoptimized: true,
-  },
+  }
 };
-
 module.exports = nextConfig;
